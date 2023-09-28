6ff4a133e68d900672b12bb3f0ae161e75dfb2d0
Ezequielgrz <ezemngrz@gmail.com>
2023-09-28 17:51:50 -0300

    cracion de paginas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,26 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'perro',
+    loadChildren: () => import('./pages/perro/perro.module').then( m => m.PerroPageModule)
+  },
+  {
+    path: 'armadura',
+    loadChildren: () => import('./pages/armadura/armadura.module').then( m => m.ArmaduraPageModule)
+  },
+  {
+    path: 'piedra',
+    loadChildren: () => import('./pages/piedra/piedra.module').then( m => m.PiedraPageModule)
+  },
+  {
+    path: 'pizarron',
+    loadChildren: () => import('./pages/pizarron/pizarron.module').then( m => m.PizarronPageModule)
+  },
+  {
+    path: 'computadora',
+    loadChildren: () => import('./pages/computadora/computadora.module').then( m => m.ComputadoraPageModule)
   }
 ];
 @NgModule({
